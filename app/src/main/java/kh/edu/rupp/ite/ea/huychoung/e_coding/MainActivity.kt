6a6e35790a7e40ffce3b35ac2da302f81ab1bc06
choung007 <120297210+choung007@users.noreply.github.com>
2023-10-03 10:27:16 +0700

    Initail commit

---
+++ app/src/main/java/kh/edu/rupp/ite/ea/huychoung/e_coding/MainActivity.kt
@@ -0,0 +1,11 @@
+package kh.edu.rupp.ite.ea.huychoung.e_coding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
